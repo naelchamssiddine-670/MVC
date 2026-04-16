df40fc63c207a2de62417081a59b8c430652443f
naelchamssiddine-670 <naelchamssiddine@gmail.com>
2026-04-16 11:05:26 +0300

    mon nouveau MVC

---
+++ app.js
@@ -0,0 +1,4 @@
+/**
+ * Le fichier app.js est une application de type expressjs
+ */
+const express = require("express");
---
+++ myserver.js
@@ -0,0 +1,21 @@
+/**
+ * le fichier myserver.js a pour mission de créer un serveur de l'application.
+ */
+
+// J'importe le package http
+const http = require("http");
+
+
+// J'importe le fichier app.js
+const app = require("./app");
+
+
+// Je déclare une variable app qui importe le fichier app.js
+const server = http.createServer(app);
+
+const numeroPort = 3000;
+
+// Je lance le serveur sur le port 3000
+server.listen(numeroPort, () => {
+    console.log(`Le serveur est à l'écoute sur le port ${numeroPort}`);
+});
